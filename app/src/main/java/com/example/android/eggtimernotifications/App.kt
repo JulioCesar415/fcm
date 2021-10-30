636235369920d2238af6353d2ef6fdb3607cab87
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-10-30 16:56:04 -0700

    create app file

--- app/src/main/java/com/example/android/eggtimernotifications/App.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/App.kt
@@ -1,2 +1,31 @@
 package com.example.android.eggtimernotifications
 
+import android.app.Application
+import timber.log.Timber
+
+class App: Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(object : Timber.DebugTree(){
+
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                super.log(priority, "global_tag_$tag", message, t)
+            }
+
+//            create tag
+            override fun createStackElementTag(element: StackTraceElement): String? {
+                return String.format(
+                    "%s:%s",
+                    element.methodName,
+                    element.lineNumber,
+                    super.createStackElementTag(element)
+                )
+            }
+        })
+
+//        Usage
+        Timber.d("App Created")
+    }
+}
